f840a4d5f23f12897f1bb620346a642db066940c
AlexSabanov <7salmin7@gmail.com>
2023-04-07 16:41:47 +0400

    added title processing and deactivation of the delete button when deselecting

--- GUI_actions.py
+++ GUI_actions.py
@@ -22,7 +22,7 @@ class PathLabel(Ui_MainWindow):
             # Добавляем выбранную папку в поле для ввода
             self._append_path_in_label(path)
 
-    def _append_path_in_label(self, path):
+    def _append_path_in_label(self, path) -> None:
         # Добавляет значение в поле "путь для поиска"
         self.path_label.append(path)
         if path != self.actual_path:
@@ -45,11 +45,11 @@ class MainGUIActions(QtWidgets.QMainWindow, PathLabel, Ui_MainWindow):
         self.select_btn.clicked.connect(self.activates_select_button)
         self.search_btn.clicked.connect(self.activates_search_button)
 
-    def activates_select_button(self):
+    def activates_select_button(self) -> None:
         """ Функция активации кнопки выбора 'пути поиска' """
         self._choise_path_in_explorer()
 
-    def activates_search_button(self):
+    def activates_search_button(self) -> None:
         path = self.get_value_path_text
         results_dictionary = search_starts(f'{path}/')
         if results_dictionary:
@@ -59,10 +59,10 @@ class MainGUIActions(QtWidgets.QMainWindow, PathLabel, Ui_MainWindow):
             self.change_process_label("Nothing found")
             self.search_btn.setEnabled(False)
 
-    def change_process_label(self, message):
+    def change_process_label(self, message) -> None:
         self.process_label.setText(message)
 
-    def open_new_window(self, results_dictionary: dict):
+    def open_new_window(self, results_dictionary: dict) -> None:
         self.ResultWindow = ResultGUIActions(results_dictionary)
         self.ResultWindow.show()
 
@@ -72,10 +72,11 @@ class ResultGUIActions(QtWidgets.QMainWindow, Ui_ResultWindow):
         self.results_dic = results_dic
         super().__init__()
         self.setupUi(self)
-        self.form_change()
+        self.changes_form()
+        self.changes_title(self.results_dic)
         self.work_path = ''
 
-    def form_change(self):
+    def changes_form(self):
         self.update_values_original_in_form()
         self.original_listWidget.itemClicked.connect(self.shows_result_duplicates)
         self.original_listWidget.itemDoubleClicked.connect(self.open_file_in_program)
@@ -83,15 +84,15 @@ class ResultGUIActions(QtWidgets.QMainWindow, Ui_ResultWindow):
         self.dup_listWidget.itemClicked.connect(self.get_full_path)
         self.del_pushButton.pressed.connect(self.move_to_trash)
 
-    def get_full_path(self, item):
+    def get_full_path(self, item) -> None:
         """ Получает имя файла и записывает его в переменную """
         self.work_path = item.text()
         self.activates_delete_button(True)
 
-    def activates_delete_button(self, flag: bool):
+    def activates_delete_button(self, flag: bool) -> None:
         self.del_pushButton.setEnabled(flag)
 
-    def update_values_original_in_form(self):
+    def update_values_original_in_form(self) -> None:
         """ Добавляет в форму значения оригиналов из словаря """
         self.original_listWidget.clear()
         if self.results_dic:
@@ -99,14 +100,15 @@ class ResultGUIActions(QtWidgets.QMainWindow, Ui_ResultWindow):
         else:
             ResultGUIActions.close(self)
 
-    def move_to_trash(self):
+    def move_to_trash(self) -> None:
         """ Перемещает файл в корзину """
         QFile.moveToTrash(self.work_path)
         update_duplicate_list = self.return_update_duplicates_list(self.work_path)
         self.show_update_duplicates_list(update_duplicate_list)
+        self.changes_title(self.results_dic)
         self.activates_delete_button(False)
 
-    def remove_key_without_values(self):
+    def remove_key_without_values(self) -> None:
         """ Удаляет ключ из словаря с пустым значением """
         dic = self.results_dic
         self.results_dic = dict(filter(lambda x: x[1], dic.items()))
@@ -125,16 +127,32 @@ class ResultGUIActions(QtWidgets.QMainWindow, Ui_ResultWindow):
                 else:
                     continue
 
-    def show_update_duplicates_list(self, dup_list: list):
+    def show_update_duplicates_list(self, dup_list: list) -> None:
         self.dup_listWidget.clear()
         self.dup_listWidget.addItems(dup_list)
 
-    def shows_result_duplicates(self, item):
+    def shows_result_duplicates(self, item) -> None:
         self.dup_listWidget.clear()
         key = item.text()
         self.dup_listWidget.addItems(self.results_dic[key])
+        self.del_pushButton.setEnabled(False)
 
-    def open_file_in_program(self, item):
+    def changes_title(self, dic: dict) -> None:
+        original_count = len(dic)
+        dup_count = self.counting_duplicates(dic)
+        self.title_label.setText(f"Found {original_count} files and {dup_count} duplicates")
+
+    def counting_duplicates(self, dic: dict) -> int:
+        dup_count = []
+        for i in dic.values():
+            if len(i) != 1:
+                for item in i:
+                    dup_count.append(item)
+            else:
+                dup_count.append(i)
+        return len(dup_count)
+
+    def open_file_in_program(self, item) -> None:
         """ Открывает файлы в программе по умолчанию Windows """
         os.startfile(item.text())
 
